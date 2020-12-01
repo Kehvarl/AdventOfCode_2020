a0daaf8eb96f698bbf000e730b4b2afe68860f87
Adam Corbett <kehvarl@gmail.com>
2020-11-30 22:24:44 -0700

    Day1 Way faster with itertools

--- 01.2/Main.py
+++ 01.2/Main.py
@@ -1,12 +1,10 @@
-
+from itertools import combinations
 
 with open("input.txt") as f:
     content = f.readlines()
 
 content = [int(num.strip()) for num in content]
 
-for v1 in content:
-    for v2 in content:
-        for v3 in content:
-            if v1 + v2 + v3 == 2020:
-                print(v1, v2, v3, (v1*v2*v3))
+for v1,v2,v3 in combinations(content, 3):
+    if v1 + v2 + v3 == 2020:
+        print(v1, v2, v3, (v1 * v2 * v3))
