c697d63148706a142fab434aa6df2cb7ebda1b67
Adam Corbett <kehvarl@gmail.com>
2020-12-01 22:11:37 -0700

    Day2 Pep9

--- 02.2/Main.py
+++ 02.2/Main.py
@@ -1,26 +1,21 @@
 
 
 with open("input.txt") as f:
-    input = f.readlines()
+    data_in = f.readlines()
 
 valid = 0
-for line in input:
+for line in data_in:
     stuff, password = line.split(':')
     nums, letter = stuff.split(' ')
     num_min, num_max = nums.split('-')
     num_min = int(num_min)
     num_max = int(num_max)
     password = password.strip()
-    pos1 = password[num_min-1] == letter
-    pos2 = password[num_max-1] == letter
+    pos1 = password[num_min - 1] == letter
+    pos2 = password[num_max - 1] == letter
 
     if pos2 ^ pos1:
         valid += 1
 
 
 print(valid)
-
-
-
-
-
