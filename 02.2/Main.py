c3cbff9e9a1bb13904f274f8becd15c0bac8a2f7
Adam Corbett <kehvarl@gmail.com>
2020-12-01 22:17:39 -0700

    Day2 simplify

--- 02.2/Main.py
+++ 02.2/Main.py
@@ -1,20 +1,12 @@
-
+import re
 
 with open("input.txt") as f:
     data_in = f.readlines()
 
 valid = 0
 for line in data_in:
-    stuff, password = line.split(':')
-    nums, letter = stuff.split(' ')
-    num_min, num_max = nums.split('-')
-    num_min = int(num_min)
-    num_max = int(num_max)
-    password = password.strip()
-    pos1 = password[num_min - 1] == letter
-    pos2 = password[num_max - 1] == letter
-
-    if pos2 ^ pos1:
+    pos1, pos2, letter, skip, password = re.split('[: \-]', line)
+    if (password[int(pos1) - 1] == letter) ^ (password[int(pos2) - 1] == letter):
         valid += 1
 
 
