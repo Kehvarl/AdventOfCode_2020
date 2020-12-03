25b4b05a834f92c3102d3c56cefab48f4a12f1ec
Adam Corbett <kehvarl@gmail.com>
2020-12-02 22:21:47 -0700

    Day3 rough solutions

--- 03.1/Main.py
+++ 03.1/Main.py
@@ -2,3 +2,19 @@
 
 with open("input.txt") as f:
     data_in = f.readlines()
+
+
+def is_impact(x, y):
+    return data_in[y][x]
+
+y = 0
+x = 0
+tree = 0
+
+while y < len(data_in):
+    if is_impact(x, y) == '#':
+        tree += 1
+    x = (x + 3) % (len(data_in[0]) -1)
+    y += 1
+
+print(tree)
--- 03.2/Main.py
+++ 03.2/Main.py
@@ -1,4 +1,32 @@
 
-
 with open("input.txt") as f:
     data_in = f.readlines()
+
+
+def is_impact(x, y):
+    return data_in[y][x]
+
+
+def check_slope(r, d):
+    y = 0
+    x = 0
+    tree = 0
+
+    while y < len(data_in):
+        if is_impact(x, y) == '#':
+            tree += 1
+        x = (x + r) % (len(data_in[0]) - 1)
+        y += d
+
+    return tree
+
+tests = []
+tests.append(check_slope(1, 1))
+tests.append(check_slope(3, 1))
+tests.append(check_slope(5, 1))
+tests.append(check_slope(7, 1))
+tests.append(check_slope(1, 2))
+
+print(tests)
+print(tests[0] * tests[1] * tests[2] * tests[3] * tests[4])
+
