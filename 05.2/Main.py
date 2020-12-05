d4b2ffff4cde7e98d88d37ea729489845fea5674
Adam Corbett <kehvarl@gmail.com>
2020-12-04 22:44:21 -0700

    Day 5 cleanup and pep8

--- 05.2/Main.py
+++ 05.2/Main.py
@@ -1,5 +1,4 @@
 
-
 with open("input.txt") as f:
     content = f.readlines()
 
@@ -8,21 +7,13 @@ def seat_decode(boarding_pass):
     row = boarding_pass[:7]
     col = boarding_pass[7::]
 
-    row = row.replace("B", "1")
-    row = row.replace("F", "0")
-    row = int(row, 2)
-
-    col = col.replace("L", "0")
-    col = col.replace("R", "1")
-    col = int(col, 2)
+    row = int(row.replace("B", "1").replace("F", "0"), 2)
+    col = int(col.replace("L", "0").replace("R", "1"), 2)
 
     return row, col, (row * 8 + col)
 
 
-seats = []
-for boarding in content:
-    seat = seat_decode(boarding)
-    seats.append(seat[2])
+seats = [seat_decode(b)[2] for b in content]
 
 seats.sort()
 
@@ -31,4 +22,3 @@ for s in range(len(seats)-1):
         print(seats[s] + 1)
 
 print(max(seats))
-
