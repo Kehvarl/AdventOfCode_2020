cd43bbd928fe3f4f46dbd4ca31ee50291d10476c
Adam Corbett <kehvarl@gmail.com>
2020-12-05 22:26:17 -0700

    Day 6 Solutions

--- 06.1/Main.py
+++ 06.1/Main.py
@@ -1,5 +1,20 @@
 
-
 with open("input.txt") as f:
     content = f.readlines()
+
+groups = []
+
+current = set()
+for line in content:
+    if line.strip() == '':
+        groups.append(current)
+        current = set()
+    else:
+        current.update(line.strip())
+
+answers = [len(x) for x in groups]
+print(groups)
+print(answers)
+print(sum(answers))
+
 
--- 06.2/Main.py
+++ 06.2/Main.py
@@ -1,5 +1,17 @@
 
-
 with open("input.txt") as f:
     content = f.readlines()
 
+groups = []
+default = set()
+default.update('abcdefghijklmnopqrstuvwxyz')
+current = default
+
+for line in content:
+    if line.strip() == '':
+        groups.append(current)
+        current = default
+    else:
+        current = current.intersection(line.strip())
+
+print(sum([len(x) for x in groups]))
