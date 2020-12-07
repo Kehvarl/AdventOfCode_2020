8499703a5fb957366a76e7e4526ed46a89e0af12
Adam Corbett <kehvarl@gmail.com>
2020-12-06 22:50:04 -0700

    Day 7 solutions

--- 07.1/Main.py
+++ 07.1/Main.py
@@ -3,3 +3,30 @@ with open("input.txt") as f:
     content = f.readlines()
 
 
+desired_bag = set()
+desired_bag.add('shiny gold')
+bag_rules = {}
+
+for rule in content:
+    bag, contains = rule.split('contain')
+    bag = bag.strip().replace(' bags', '')
+    if bag not in bag_rules:
+        bag_rules[bag] = {}
+
+    for allowed in contains.split(','):
+        if allowed.strip()[:2] != "no":
+            number = allowed[1]
+            color = allowed[2::].strip().replace('.', '').replace(' bags', '').replace(' bag', '')
+            if color not in bag_rules[bag]:
+                bag_rules[bag][color] = 0
+            bag_rules[bag][color] += int(number)
+
+for x in range(50):
+    for rule in bag_rules:
+        for bag in bag_rules[rule]:
+            if bag in desired_bag:
+                desired_bag.add(rule)
+
+print(desired_bag)
+print(len(desired_bag))
+
--- 07.2/Main.py
+++ 07.2/Main.py
@@ -3,3 +3,38 @@ with open("input.txt") as f:
     content = f.readlines()
 
 
+desired_bag = set()
+desired_bag.add('shiny gold')
+bag_rules = {}
+
+for rule in content:
+    bag, contains = rule.split('contain')
+    bag = bag.strip().replace(' bags', '')
+    if bag not in bag_rules:
+        bag_rules[bag] = {}
+
+    for allowed in contains.split(','):
+        if allowed.strip()[:2] != "no":
+            number = allowed[1]
+            color = allowed[2::].strip().replace('.', '').replace(' bags', '').replace(' bag', '')
+            if color not in bag_rules[bag]:
+                bag_rules[bag][color] = 0
+            bag_rules[bag][color] += int(number)
+
+
+def decode_bag(color):
+    if len(bag_rules[color]) == 0:
+        return 0
+
+    print(bag_rules[color])
+    total = 0
+    for bag in bag_rules[color]:
+        bags = bag_rules[color][bag]
+        inside = decode_bag(bag)
+        total += bags + bags * inside
+
+    return total
+
+
+print(decode_bag('shiny gold'))
+# print(decode_bag('dark olive'))
