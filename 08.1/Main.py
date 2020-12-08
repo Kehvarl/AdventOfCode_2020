415bc9bbfe7d09e3a71575b8230ba533799b7ea0
Adam Corbett <kehvarl@gmail.com>
2020-12-07 22:35:03 -0700

    Day 8 Solutions

--- 08.1/Main.py
+++ 08.1/Main.py
@@ -1,3 +1,24 @@
 
 with open("input.txt") as f:
     content = f.readlines()
+
+
+acc = 0
+running = True
+line = 0
+executed = set()
+while running:
+    if line >= len(content) or line in executed:
+        break
+    operation, argument = content[line].strip().split(' ')
+    executed.add(line)
+
+    if operation == "nop":
+        line += 1
+    elif operation == "acc":
+        acc += int(argument)
+        line += 1
+    elif operation == "jmp":
+        line += int(argument)
+
+print(acc)
--- 08.2/Main.py
+++ 08.2/Main.py
@@ -1,3 +1,43 @@
 
 with open("input.txt") as f:
     content = f.readlines()
+
+
+running = True
+swap = 0
+acc = 0
+
+while running:
+    acc = 0
+    line = 0
+    executed = set()
+    while True:
+        if line >= len(content):
+            running = False
+            print("swapped: ", swap)
+            break
+        if line in executed:
+            swap += 1
+            print(line)
+            break
+        operation, argument = content[line].strip().split(' ')
+
+        if swap == line:
+            if operation == "jmp":
+                operation = "nop"
+            elif operation == "nop":
+                operation = "jmp"
+
+        executed.add(line)
+        print(line, operation, argument)
+
+        if operation == "nop":
+            line += 1
+        elif operation == "acc":
+            acc += int(argument)
+            line += 1
+        elif operation == "jmp":
+            line += int(argument)
+
+
+print(acc)
