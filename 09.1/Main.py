f256b8f46b5e06b20942f66c1416e3e4968f0d2a
Adam Corbett <kehvarl@gmail.com>
2020-12-08 22:25:53 -0700

    Day 9 solutions

--- 09.1/Main.py
+++ 09.1/Main.py
@@ -1,3 +1,17 @@
 
 with open("input.txt") as f:
-    content = f.readlines()
+    content = [int(x) for x in f.readlines()]
+
+
+preamble = 25
+
+for current in range(25, len(content)):
+    cval = content[current]
+    window = content[current - 25:current]
+    for previous in range(25):
+        pval = content[current - previous]
+        if (cval - pval) in window:
+            break
+    else:
+        print(cval)
+
--- 09.2/Main.py
+++ 09.2/Main.py
@@ -1,3 +1,16 @@
 
 with open("input.txt") as f:
-    content = f.readlines()
+    content = [int(x) for x in f.readlines()]
+
+
+preamble = 25
+target = 400480901
+
+for start in range(len(content)):
+    for end in range(1, len(content)):
+        if sum(content[start:end]) == target:
+            print (min(content[start:end]), max(content[start:end]), min(content[start:end]) + max(content[start:end]))
+        elif sum(content[start:end]) > target:
+            break
+
+
