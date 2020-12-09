89461fbfff55b393d1d3a6eb0cd452505cd6596d
Adam Corbett <kehvarl@gmail.com>
2020-12-08 22:38:39 -0700

    Day9 pep8

--- 09.2/Main.py
+++ 09.2/Main.py
@@ -9,8 +9,6 @@ target = 400480901
 for start in range(len(content)):
     for end in range(1, len(content)):
         if sum(content[start:end]) == target:
-            print (min(content[start:end]), max(content[start:end]), min(content[start:end]) + max(content[start:end]))
+            print(min(content[start:end]), max(content[start:end]), min(content[start:end]) + max(content[start:end]))
         elif sum(content[start:end]) > target:
             break
-
-
