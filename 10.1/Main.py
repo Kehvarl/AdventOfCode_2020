3b0c7f8dadc0dfb66b88a0b85805999d189e3214
Adam Corbett <kehvarl@gmail.com>
2020-12-09 23:00:51 -0700

    Day 10 Solutions and Pep8

--- 10.1/Main.py
+++ 10.1/Main.py
@@ -2,3 +2,17 @@
 with open("input.txt") as f:
     content = [int(x) for x in f.readlines()]
 
+current_joltage = 0
+content.sort()
+transitions = {1: 0, 2: 0, 3: 0}
+for adapter in content:
+    if adapter > current_joltage + 3:
+        print("Broken Chain: ", current_joltage, adapter)
+        break
+    transitions[adapter - current_joltage] += 1
+    current_joltage = adapter
+
+transitions[3] += 1
+
+print(transitions)
+print(transitions[1] * transitions[3])
--- 10.2/Main.py
+++ 10.2/Main.py
@@ -2,3 +2,17 @@
 with open("input.txt") as f:
     content = [int(x) for x in f.readlines()]
 
+content.append(max(content) + 3)
+content.sort()
+
+
+memo = {0: 1}
+
+for v in content:
+    memo[v] = 0
+    for n in [1, 2, 3]:
+        if v - n in memo:
+            memo[v] += memo[v - n]
+
+print(memo[max(content)])
+print(memo)
