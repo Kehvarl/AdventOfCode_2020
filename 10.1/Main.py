7eea1cb6ad10521f691683b3ea5cdbfb9a8c3581
Adam Corbett <kehvarl@gmail.com>
2020-12-09 23:12:15 -0700

    Day 10 Solutions and Pep8

--- 10.1/Main.py
+++ 10.1/Main.py
@@ -4,15 +4,10 @@ with open("input.txt") as f:
 
 current_joltage = 0
 content.sort()
-transitions = {1: 0, 2: 0, 3: 0}
+transitions = {1: 0, 2: 0, 3: 1}
 for adapter in content:
-    if adapter > current_joltage + 3:
-        print("Broken Chain: ", current_joltage, adapter)
-        break
     transitions[adapter - current_joltage] += 1
     current_joltage = adapter
-
-transitions[3] += 1
 
 print(transitions)
 print(transitions[1] * transitions[3])
