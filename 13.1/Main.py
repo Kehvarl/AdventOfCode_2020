21ea624050c44e0034f8bf7eba393eaaee6375b0
Adam Corbett <kehvarl@gmail.com>
2020-12-12 23:00:23 -0700

    Day 13. Solution and Pep8

--- 13.1/Main.py
+++ 13.1/Main.py
@@ -1,3 +1,27 @@
 
 with open("input.txt") as f:
-    content = f.readlines()
+    timestamp = int(f.readline())
+    bus_ids = [int(x) for x in f.readline().split(',') if x.isnumeric()]
+
+
+min_bus = 99
+bus_delta = 1
+
+print(bus_ids)
+
+for id in bus_ids:
+    if ((timestamp // id) +1) - (timestamp / id) < bus_delta:
+        bus_delta = ((timestamp // id) + 1) - (timestamp / id)
+        min_bus = id
+    print(id, timestamp // id, timestamp / id)
+
+
+diff = 1
+print(timestamp)
+print(min_bus, timestamp // min_bus, timestamp / min_bus)
+while (timestamp + diff) % min_bus != 0:
+    diff += 1
+print(diff, min_bus, diff * min_bus)
+print(timestamp + diff, (timestamp + diff) / min_bus)
+
+
--- 13.2/Main.py
+++ 13.2/Main.py
@@ -1,3 +1,48 @@
+from functools import reduce
 
 with open("input.txt") as f:
-    content = f.readlines()
+    ignore = f.readline()
+    bus_ids = [x for x in f.readline().strip().split(',')]
+
+
+def chinese_remainder(cr_n, cr_a):
+    """
+    From Rosetta Code
+    https://rosettacode.org/wiki/Chinese_remainder_theorem#Python_3.6
+    """
+    cr_sum = 0
+    prod = reduce(lambda a, b: a * b, cr_n)
+    for n_i, a_i in zip(cr_n, cr_a):
+        p = prod // n_i
+        cr_sum += a_i * mul_inv(p, n_i) * p
+    return cr_sum % prod
+
+
+def mul_inv(mi_a, mi_b):
+    """
+    From Rosetta Code
+    https://rosettacode.org/wiki/Chinese_remainder_theorem#Python_3.6
+    """
+    b0 = mi_b
+    x0, x1 = 0, 1
+    if mi_b == 1:
+        return 1
+    while mi_a > 1:
+        q = mi_a // mi_b
+        mi_a, mi_b = mi_b, mi_a % mi_b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0:
+        x1 += b0
+    return x1
+
+
+bus = []
+interval = []
+
+for i in range(len(bus_ids)):
+    if bus_ids[i].isnumeric():
+        temp_bus = int(bus_ids[i])
+        bus.append(temp_bus)
+        interval.append((temp_bus - i) % temp_bus)
+
+print(chinese_remainder(bus, interval))
