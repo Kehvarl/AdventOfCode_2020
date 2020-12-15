d262cc947d893c3a5fa3a68584a36adb2883b0b7
Adam Corbett <kehvarl@gmail.com>
2020-12-14 22:51:58 -0700

    Day 15. Some cleanup

--- 15.2/Main.py
+++ 15.2/Main.py
@@ -9,19 +9,15 @@ last = None
 for turn in range(30000000):
     if content:
         last = content.pop()
-        if last not in nums:
-            nums[last] = [turn]
-        else:
-            nums[last].append(turn)
     else:
         if len(nums[last]) == 1:
             last = 0
         else:
             last = nums[last][-1] - nums[last][-2]
 
-        if last in nums:
-            nums[last].append(turn)
-        else:
-            nums[last] = [turn]
+    if last not in nums:
+        nums[last] = [turn]
+    else:
+        nums[last].append(turn)
 
 print(last)
