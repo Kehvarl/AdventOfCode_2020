68a25b95f1e6156f874a646d37f12814274ca211
Adam Corbett <kehvarl@gmail.com>
2020-12-17 23:27:42 -0700

    Day 18 solution

--- 18.1/Main.py
+++ 18.1/Main.py
@@ -1,3 +1,47 @@
 
 with open("input.txt") as f:
     content = f.readlines()
+
+
+def operate(accumulator, operator, operand):
+    operand = int(operand)
+    if operator is None:
+        accumulator = operand
+    elif operator == '+':
+        accumulator += operand
+    elif operator == '-':
+        accumulator -= operand
+    elif operator == '*':
+        accumulator *= operand
+    elif operator == '/':
+        accumulator /= operand
+    return accumulator
+
+def parse_line(line):
+    accum = 0
+    operation = None
+    i = 0
+
+    while i < len(line):
+        char = line[i]
+        if char == "(":
+            a, ti = parse_line(line[i+1:])
+            accum = operate(accum, operation, a)
+            i = i + ti
+        elif char == ")":
+            return accum, i+1
+        elif char in ['+', '-', '*', '/']:
+            operation = char
+        elif char.isnumeric():
+            accum = operate(accum, operation, int(char))
+
+        i += 1
+
+    return accum, i
+
+
+total = 0
+for line in content:
+    a, i = parse_line(line)
+    total += a
+print(total)
--- 18.2/Main.py
+++ 18.2/Main.py
@@ -1,3 +1,34 @@
+import re
+import operator
 
 with open("input.txt") as f:
     content = f.readlines()
+
+
+GROUP = re.compile(r'\(([^)(]+)\)')
+ADD = re.compile(r'(\d+\s+\+\s+\d+)')
+OPS = {'+': operator.add, '*': operator.mul}
+
+
+def calc(expr, add=False):
+    grouping = GROUP.findall(expr)
+    while grouping:
+        expr = GROUP.sub(calc(grouping[0], add=add), expr, 1)
+        grouping = GROUP.findall(expr)
+    if add:
+        addition = ADD.findall(expr)
+        while addition:
+            expr = ADD.sub(calc(addition[0], add=False), expr, 1)
+            addition = ADD.findall(expr)
+
+    terms = expr.split()
+    result = int(terms.pop(0))
+    for _ in range(len(terms)//2):
+        result = OPS[terms.pop(0)](result, int(terms.pop(0)))
+    return str(result)
+
+
+total = 0
+for line in content:
+    total += int(calc(line, add=True))
+print(total)
