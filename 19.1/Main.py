6731540cf2369d0bdb1b2ba1c85f7768a4e7d0c6
Adam Corbett <kehvarl@gmail.com>
2020-12-18 22:52:53 -0700

    Day 19 failure many of many

--- 19.1/Main.py
+++ 19.1/Main.py
@@ -1,3 +1,41 @@
 
-with open("input.txt") as f:
+with open("test.txt") as f:
     content = f.readlines()
+
+rules = {}
+lines = []
+for line in content:
+    if ':' in line:
+        rule, requirements = line.strip().split(": ")
+        requirements = [x.strip().strip('"') for x in requirements.split("|")]
+        rules[int(rule)] = requirements
+    else:
+        lines.append(line)
+
+for index in rules:
+    r = []
+    for rule in rules[index]:
+        if rule[0].isnumeric():
+            r.append([int(x) for x in rule.split(" ")])
+        else:
+            r = rule
+    rules[index] = r
+
+
+def rule_solver(rule):
+    if type(rule) == str:
+        return rule
+    if type(rule) == int:
+        return rule_solver(rules[rule])
+
+    a, b = rule
+    a = rule_solver(a)
+    b = rule_solver(b)
+    return [a, b]
+
+
+goal = rules[0][:]
+print(rules)
+print(goal[0])
+s = rule_solver(goal[0])
+solutions = set()
