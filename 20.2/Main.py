bcf0d1b98f003bb311e4ed010d2037df9da2bcd7
Adam Corbett <kehvarl@gmail.com>
2020-12-20 00:32:55 -0700

    Day 20 will the pain never end?

--- 20.2/Main.py
+++ 20.2/Main.py
@@ -1,3 +1,4 @@
+import collections
 
 with open("test.txt") as f:
     content = f.readlines()
@@ -10,28 +11,20 @@ tiles = {}
 tiles_edges = {}
 
 tile_num = 0
-t_line = 0
 
 for line in content:
     if line.startswith("Tile"):
         tile_num = int(line.strip().strip(":").split(" ")[1])
-        t_line = 0
         tiles[tile_num] = []
         tiles_edges[tile_num] = ["", "", "", ""]
     elif line.strip() == '':
         continue
     else:
         tiles[tile_num].append(line.strip())
-        if t_line == 0:
-            tiles_edges[tile_num][0] = line.strip()
-        if t_line == 9:
-            tiles_edges[tile_num][1] = line.strip()
-            tiles_edges[tile_num][2] += line.strip()[0]
-            tiles_edges[tile_num][3] += line.strip()[-1]
-        else:
-            tiles_edges[tile_num][2] += line.strip()[0]
-            tiles_edges[tile_num][3] += line.strip()[-1]
-        t_line += 1
+
+for tile in tiles:
+    lines = tiles[tile]
+    tiles_edges[tile] = [''.join(l[0] for l in lines)[::-1], lines[0], ''.join(l[-1] for l in lines), lines[-1][::-1]]
 
 
 def match_tile(tile_a, tile_b):
@@ -62,19 +55,73 @@ for tile_id, tile in tiles.items():
         tile[index] = line[1:-1]
 
 
-def flip(flip_tile):
-    for line_index, tile_line in enumerate(flip_tile):
-        flip_tile[line_index] = tile_line[::-1]
+# def flip(flip_tile):
+#     for line_index, tile_line in enumerate(flip_tile):
+#         flip_tile[line_index] = tile_line[::-1]
+#     return flip_tile
+#
+#
+# def rotate(rotate_tile):
+#     new_tile = ["" for _ in range(len(rotate_tile))]
+#     for line_index, tile_line in enumerate(rotate_tile):
+#         for c_index, char in enumerate(tile_line.split()):
+#             new_tile[c_index] += char
+#     return flip(new_tile)
 
 
-def rotate(rotate_tile):
-    new_tile = ["" for _ in range(len(tile))]
-    for line_index, tile_line in rotate_tile:
-        for c_index, char in enumerate(tile_line.split()):
-            new_tile[c_index] += char
-    return new_tile
+def flip(lines):
+    return[l[::-1] for l in lines]
 
 
-image = []
-for k, m in matches.items():
-    pass
+def rotate(lines):
+    s = []
+    for row in range(len(lines)):
+        s.append(''.join(l[-1-row] for l in lines))
+    return s
+
+
+def edges(lines):
+    return [''.join(l[0] for l in lines)[::-1], lines[0], ''.join(l[-1] for l in lines), lines[-1][::-1]]
+
+
+edgecount = collections.defaultdict(int)
+edgetotile = collections.defaultdict(list)
+
+for head, lines in tiles.items():
+    ee = edges(lines)  # (left, top, right, bottom)
+    for e in ee:
+        e = min(e, e[::-1])
+        edgecount[e] += 1
+        edgetotile[e].append(head)
+
+
+used = set()
+
+assembly = [[]]
+
+
+def go():
+    p = 1
+    for head, lines in tiles.items():
+
+        if len(matches[head]) == 2:
+            p *= head
+
+            # for part 1, remove the following code:
+            ll = lines
+            for _ in range(4):
+                e = edges(ll)
+                if edgecount[min(e[0], e[0][::-1])] == 1 and edgecount[min(e[1], e[1][::-1])] == 1:
+                    assembly[0].append(ll)
+                    used.add(head)
+                    return
+                ll = flip(ll)
+                if edgecount[min(e[0], e[0][::-1])] == 1 and edgecount[min(e[1], e[1][::-1])] == 1:
+                    assembly[0].append(ll)
+                    used.add(head)
+                    return
+                ll = flip(ll)
+                ll = rotate(ll)
+            assert False
+go()
+
