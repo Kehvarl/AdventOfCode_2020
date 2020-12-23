4a4aed3c06ebc007cdccb3a3c238ac30f04016c6
Adam Corbett <kehvarl@gmail.com>
2020-12-22 23:31:10 -0700

    Day 23. No

--- 23.1/Main.py
+++ 23.1/Main.py
@@ -1,3 +1,26 @@
 
-with open("input.txt") as f:
-    content = f.readlines()
+data = [1, 3, 5, 4, 6, 8, 7, 2, 9]
+
+test = [3, 8, 9, 1, 2, 5, 4, 6, 7]
+
+cups = data[:]
+
+for _ in range(100):
+    current = cups[0]
+    removed = cups[1:4]
+    working_set = cups[4:]
+
+    destination = current - 1
+    while destination not in working_set:
+        destination -= 1
+        if destination < min(cups):
+            destination = max(cups)
+
+    index = working_set.index(destination)
+    cups = working_set[:index + 1] + removed + working_set[index + 1:] + [current]
+
+print(''.join([str(x) for x in cups[1:]]))
+
+
+
+
--- 23.2/Main.py
+++ 23.2/Main.py
@@ -1,3 +1,45 @@
+from collections import deque, defaultdict
 
-with open("input.txt") as f:
-    content = f.readlines()
+data = [1, 3, 5, 4, 6, 8, 7, 2, 9]
+
+test = [3, 8, 9, 1, 2, 5, 4, 6, 7]
+
+cups = data[:] + list(range(max(data)+1, 1000001))
+
+input = cups
+current = input[0]
+ring = {}
+for i in range(len(input)):
+    if i == len(input) - 1:
+        ring[input[i]] = input[0]
+    else:
+        ring[input[i]] = input[i+1]
+
+
+def round(ring,current):
+    maximum = 1000000
+    minimum = 1
+    next = ring[current]
+    outside = [None,None,None]
+    for x in range(3):
+        outside[x] = next
+        next = ring[next]
+    ring[current] = next
+    dest = current - 1
+    if dest == 0:
+        dest = maximum
+    while dest in outside:
+        if dest > minimum:
+            dest = dest - 1
+        else:
+            dest = maximum
+    stitch = ring[dest]
+    ring[dest] = outside[0]
+    ring[outside[2]] = stitch
+    return ring, next
+
+
+for x in range(10000000):
+    ring,current = round(ring,current)
+
+print(ring[1]*ring[ring[1]])
