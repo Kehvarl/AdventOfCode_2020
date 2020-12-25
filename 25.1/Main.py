fa4443506a0a892585026defb0dd8caa8ae79857
Adam Corbett <kehvarl@gmail.com>
2020-12-24 22:17:03 -0700

    Day 25 Finished!

--- 25.1/Main.py
+++ 25.1/Main.py
@@ -1,3 +1,18 @@
 
-with open("input.txt") as f:
-    content = f.readlines()
+keys = [17115212, 3667832]
+# keys = [5764801, 17807724]
+
+start = 1
+subject = 7
+loop = 0
+while True:
+    start = (subject * start) % 20201227
+    if start in keys:
+        subject = [k for k in keys if k != start][0]
+        break
+    loop += 1
+
+start = 1
+for i in range(loop+1):
+    start = (subject * start) % 20201227
+print(subject, start)
